3d3d7df701f047224f38277294ce8805493602d3
felipeboffnunes <felipeboffnunes@protonmail.com>
2022-04-16 12:59:32 -0300

    save_session_data with loaded sessions

--- src/data.py
+++ src/data.py
@@ -9,6 +9,8 @@ class DataHandler:
 
     @staticmethod
     def save_session_data(book_id: int, tracker_data: pd.DataFrame) -> None:
-        book_id_str = str(book_id).zfill(4)
-        path = Path(PATHS['DATA_DIR']) / f'{book_id_str}.csv'
-        tracker_data.to_csv(path, mode='a', header=False)
+        path = Path(PATHS['DATA_DIR']) / f'{book_id:04d}.csv'
+        if path.exists():
+            previous_tracker_data = pd.read_csv(path, index_col=0)
+            tracker_data = pd.concat([previous_tracker_data, tracker_data], ignore_index=True)
+        tracker_data.to_csv(path, float_format='%.3f')
